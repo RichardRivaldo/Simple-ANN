88441f8c70d3fe25db756d8a2184413035c7521b
RichardRivaldo <richardrivaldo84@gmail.com>
2021-06-22 23:43:04 +0700

    initialized the source code

---
+++ src/ann.py
@@ -0,0 +1,9 @@
+# Implementasikan ketiga bagian dari ANN:
+# Forward Propagation
+# Back Propagation
+# Updating Weight
+# Algoritma yang dibuat, minimal dapat:
+# Menerima optimizer Stochastic Gradient Descent (SGD)
+# Menerima loss function MSE
+# Dapat menerima activation function sigmoid dan relu
+# Mengkategorikan label binary (True False)
